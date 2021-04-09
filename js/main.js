6bc4a1e696cd5717035854bd8c7ef2d375d6941b
Daniel Kim <danielkim2711@gmail.com>
2021-04-10 01:04:35 +1200

    piha

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function pihaClick() {
+    window.open('http://www.piha.co.nz', '_blank');
+}
+
+function replayBtn() {
+    var selectedDiv = document.getElementById('part-2');
+    var newDiv = selectedDiv.cloneNode(true);
+    selectedDiv.parentNode.replaceChild(newDiv, selectedDiv);
+
+}
+
 const resultsList = document.getElementById('results')
 new URLSearchParams(window.location.search).forEach((value, name) => {
     resultsList.append('')
