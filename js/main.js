73bf0536ee6e42067ee0c3ff26fb0d04cd27246d
Daniel Kim <danielkim2711@gmail.com>
2021-09-29 23:52:09 +1300

    Format CSS and JS

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 function pihaClick() {
-    window.open('http://www.piha.co.nz', '_blank');
+  window.open('http://www.piha.co.nz', '_blank');
 }
 
 function replayBtn() {
-    var selectedDiv = document.getElementById('part-2');
-    var newDiv = selectedDiv.cloneNode(true);
-    selectedDiv.parentNode.replaceChild(newDiv, selectedDiv);
-
+  var selectedDiv = document.getElementById('part-2');
+  var newDiv = selectedDiv.cloneNode(true);
+  selectedDiv.parentNode.replaceChild(newDiv, selectedDiv);
 }
 
-const summaryList = document.getElementById('summary')
+const summaryList = document.getElementById('summary');
 new URLSearchParams(window.location.search).forEach((value, name) => {
-    summaryList.append(`${name}: ${value}`)
-    summaryList.append(document.createElement('br'))
-})
+  summaryList.append(`${name}: ${value}`);
+  summaryList.append(document.createElement('br'));
+});
